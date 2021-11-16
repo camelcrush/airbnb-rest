0aae28ff72d3f8661b0b75201f8863935bdcaf31
camelcrush <dkxm0927@naver.com>
2021-11-16 20:19:57 +0900

    @api_view, set url for room list

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,9 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -132,3 +134,7 @@ MEDIA_URL = "/media/"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+# AutoField
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
