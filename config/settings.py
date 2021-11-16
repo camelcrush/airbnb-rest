83100602c58a03f0d17664f6529633fa13270b7e
camelcrush <dkxm0927@naver.com>
2021-11-16 21:11:57 +0900

    ListAPIView & Pagination: default pagination config

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,10 @@ AUTH_USER_MODEL = "users.User"
 # AutoField
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,9 @@
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 
 
-class ListRoomsView(APIView):
-    def get(self, request):
-        rooms = Room.objects.all()
-        serialzer = RoomSerializer(rooms, many=True)
-        return Response(serialzer.data)
+class ListRoomsView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
