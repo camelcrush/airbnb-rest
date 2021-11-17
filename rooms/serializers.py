ca055081af49ef284295622660a07d3ad2789fba
camelcrush <dkxm0927@naver.com>
2021-11-17 20:53:23 +0900

    Validate: validate data for update instance or not

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -30,11 +30,14 @@ class WriteRoomSerializer(serializers.Serializer):
         return Room.objects.create(**validated_data)
 
     def validate(self, data):
-        if not self.instance:
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+        else:
             check_in = data.get("check_in")
             check_out = data.get("check_out")
-            if check_in == check_out:
-                raise serializers.ValidationError("Not enough time between changes")
+        if check_in == check_out:
+            raise serializers.ValidationError("Not enough time between changes")
         return data
 
     def update(self, instance, validated_data):
